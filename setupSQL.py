5727d922139f9217e4dccbdffe8a2acfd521b674
Svante <svante.lars.sundberg@gmail.com>
2022-12-13 01:17:38 +0100

    Added docstrings and comments

--- setupSQL.py
+++ setupSQL.py
@@ -4,20 +4,30 @@ import time
 
 AWS_REGION = "us-east-1a"
 KEY_PAIR_NAME = 'vockey'
-AMI_ID = 'ami-061dbd1209944525c'  # ubuntu 18.04
+AMI_ID = 'ami-061dbd1209944525c'  # Ubuntu 18.04 LTS
 INSTANCE_TYPE = 't2.micro'
 
-
+# Create a Boto3 resource that is used for creating instances and security groups
 ec2_RESOURCE = boto3.resource('ec2', region_name='us-east-1')
-ec2_CLIENT = boto3.client('ec2')
-elb = boto3.client('elbv2')
-cloudwatch = boto3.client('cloudwatch')
 
+# Creates a Boto3 client used that is used for getting the VpcId
+ec2_CLIENT = boto3.client('ec2')
+
+# Gets the VpcId from the Boto3 client
 response = ec2_CLIENT.describe_vpcs()
 vpc_id = response.get('Vpcs', [{}])[0].get('VpcId', '')
 
 
 def create_security_group():
+    '''
+    Creates a security group in the existing vpc that allows all traffic on all ports with the name 'security_group'
+
+            Parameters:
+                    None
+
+            Returns:
+                    security_group_id (str): String with the ID of the created security group.
+    '''
     try:
         security_group = ec2_RESOURCE.create_security_group(GroupName='security_group',
                                                             Description='Allows all inbound and outbound traffic',
@@ -49,6 +59,16 @@ def create_security_group():
 
 
 def createSQLinstance(name, sg_id):
+    '''
+    Returns an Ubuntu 18.04 LTS EC2 t2.micro instance with key-pair 'vockey' in availability zone 'us-east-1a'
+
+            Parameters:
+                    name (str): A string representing the name of the instance
+                    sg_ig (str): Another string representing the security group ID
+
+            Returns:
+                    security_group_id (str): String with the ID of the created security group.
+    '''
     instance = ec2_RESOURCE.create_instances(
         ImageId=AMI_ID,
         InstanceType=INSTANCE_TYPE,
@@ -75,6 +95,14 @@ def createSQLinstance(name, sg_id):
 
 
 def main():
+    '''
+    Creates an security group and 5 different instances by calling 'create_security_group()' and 'createSQLinstance()'.
+    Prints out the private IP addresses of each instance.
+
+            Parameters: None
+
+            Returns: None
+    '''
     security_id = create_security_group()
     print('Launching instances...')
     standalone = createSQLinstance('standalone_SQL', security_id)
@@ -99,5 +127,8 @@ def main():
           Data_Node3[0].private_ip_address)
     print('Done')
 
+    print(main.__doc__)
 
+
+# Runs the main() method
 main()
